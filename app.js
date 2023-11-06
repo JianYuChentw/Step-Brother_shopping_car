f4f6bf6d1e856d8572dfedab296e8614d0722ad8
JianYuChentw <coke818572@gmail.com>
2023-11-06 10:11:47 +0800

    test sourcetree

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const session = require('express-session');
+const cors = require('cors'); //跨來源資源共享模組
+const path = require('path');
+
+// 導入swaggerUi與文件
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger-output.json');
+
+// const routes = require('./router/routers');
+const tool = require('./model/tool_model');
+// const port = 3000;
+
+// 線上部署的環境變數設定及路由
+const routes = require('./router/testRouters'); // 路由
+const port = process.env.PORT || 3000; // 埠號添加環境變數
+
+const app = express();
+// 開啟cors
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
+);
+//* 啟動ejs
+const engine = require('ejs-locals');
+app.engine('ejs', engine);
+app.set('views', './views');
+app.set('view engine', '.ejs');
+
+//架起session跟socket.io的中間橋樑
+const sessionMiddleware = session({
+  secret: 'cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: false,
+    maxAge: 1000 * 60 * 60 * 24 * 7, // 生命週期一週
+  },
+});
+
+//帶入session
+app.use(sessionMiddleware);
+
+//帶入bodyParser
+// app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/', routes);
+
+// 調用SwaggerUi必須設定於app使用express之下
+app.use('/swagger/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+//監聽port
+app.listen(port, () => {
+  console.log(`http://localhost:${port}/stepbrothers`);
+});
+
+// 監聽session時效
+
+// 監聽伺服器關閉
+process.on('SIGINT', async () => {
+  console.log('關閉伺服器');
+  try {
+    await tool.delete_all_user_status();
+    console.log('清除所有狀態完成');
+  } catch (error) {
+    console.error('清除狀態時發生錯誤:', error);
+  }
+  process.exit(0);
+});
